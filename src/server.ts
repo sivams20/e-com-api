9575c4ed2a90936c7b06c36b8192cbd7381fda7f
unknown <sivams20@gmail.com>
2024-06-05 11:00:44 +0530

    MongoDB connected

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 5000;
@@ -10,3 +11,14 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Connected successfully on port ${port}`);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://sivams20:miqRt0CcROUNAhx0@expensetrackcluster.lvlsc.mongodb.net/?retryWrites=true&w=majority&appName=expenseTrackCluster"
+  )
+  .then(() => {
+    console.log("Connected to database !!");
+  })
+  .catch((err: any) => {
+    console.log("Connection failed! " + err);
+  });
