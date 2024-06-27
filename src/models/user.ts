385bd49ed12cfbc2e051c9b0d76964b664197577
unknown <sivams20@gmail.com>
2024-06-27 11:11:03 +0530

    Login issue fixed

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   email: {
     type: String,
     required: true,
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,96 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
 import connect from "./config/dbconnect.js";
+import User from "./models/user.js";
+import flash from "connect-flash";
 
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({ secret: "yourSecret", resave: false, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "Incorrect email." });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+app.post("/login", (req: any, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash("error", info.message);
+      return res.redirect("/loginfailure");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("loginsuccess");
+    });
+  })(req, res, next);
+});
+
+app.get("/loginsuccess", (req, res) => {
+  res.send("Login success");
+});
+
+app.get("/loginfailure", (req, res) => {
+  res.send("Login Failed");
+});
+
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).send("User registered");
+  } catch (err) {
+    res.status(500).send("Error registering user");
+  }
 });
 
 app.listen(port, () => {
